c5be2f9dc22619fdebdeafa65d24e3969f4ef0db
melekmoalla <melekmoalla2468@gmail.com>
2023-02-20 14:13:09 +0100

    test work

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -4,11 +4,31 @@ import unittest
 from models.base_model import BaseModel
 
 
-class test_BaseModel(unittest.TestCase):
+class testBase_AirBnB(unittest.TestCase):
+
+    def test_save(self):
+        my_model = BaseModel()
+        self.assertEqual(my_model.save(), None)
 
     def test_to_dict(self):
         my_model = BaseModel()
         my_model.name = "My First Model"
         my_model.my_number = 89
+        my_model.save()
         my_model_json = my_model.to_dict()
-        self.assertEqual(print(my_model_json), None)
+        self.assertEqual(my_model_json, my_model_json)
+
+    def test_BaseModel_id(self):
+        my_model = BaseModel()
+        self.assertEqual(my_model.id, my_model.id)
+
+    def test_self_created_at(self):
+        my_model = BaseModel()
+        self.assertEqual(my_model.created_at, my_model.created_at)
+
+    def test_my_model_created_at(self):
+        my_model = BaseModel()
+        my_model.name = "My First Model"
+        my_model.my_number = 89
+        a = print(my_model)
+        self.assertEqual(print(my_model), a)
