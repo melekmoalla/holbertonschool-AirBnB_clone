f816d9b8f39f2471378e7b784585ab52ffaeb2d1
melekmoalla <melekmoalla2468@gmail.com>
2023-02-20 13:41:35 +0100

    test file test first hard working

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import unittest
+from models.base_model import BaseModel
+
+
+class testBase_AirBnB(unittest.TestCase):
+
+    def test_save(self):
+        my_model = BaseModel()
+        self.assertEqual(my_model.save(), None)
+
+    def test_to_dict(self):
+        my_model = BaseModel()
+        my_model.name = "My First Model"
+        my_model.my_number = 89
+        my_model.save()
+        my_model_json = my_model.to_dict()
+        self.assertEqual(my_model_json, my_model_json)
+
+    def test_BaseModel_id(self):
+        my_model = BaseModel()
+        self.assertEqual(my_model.id, my_model.id)
+
+    def test_self_created_at(self):
+        my_model = BaseModel()
+        self.assertEqual(my_model.created_at, my_model.created_at)
+
+    def test_my_model_created_at(self):
+        my_model = BaseModel()
+        my_model.name = "My First Model"
+        my_model.my_number = 89
+        self.assertEqual(str(my_model), str(my_model))
