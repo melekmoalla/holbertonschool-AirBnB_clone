08d5124dd70127be4ccf672c7cd4a3b72610cfdd
melekmoalla <melekmoalla2468@gmail.com>
2023-02-21 22:02:50 +0100

    test cmd somthing new

---
+++ console.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+    This is the console module
+"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """Command interpreter for HBNB"""
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF command to exit the program"""
+        return True
+
+    def emptyline(self):
+        """Do nothing on empty line"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
