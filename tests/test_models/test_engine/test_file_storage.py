ae74d42981cd94d193ee2d123141ad8a61baf61e
melekmoalla <melekmoalla2468@gmail.com>
2023-02-24 00:25:11 +0100

    test all another method

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -25,25 +25,26 @@ class TestFileStorage(unittest.TestCase):
     def test_save(self):
         all_objs = storage.all()
         my_model = BaseModel()
-        my_model.name = "John"
+        my_model.name = "My_First_Model"
+        my_model.my_number = 89
         
         my_model.save()
-        all_objs = storage.all()
 
         with open('file.json', 'r') as f:
             content = json.load(f)
             key = "{}.{}".format(type(my_model).__name__, my_model.id)
             self.assertEqual(content[key]['name'], my_model.name)
-            self.assertEqual(all_objs , all_objs)
+
+        my_model = BaseModel()
+        my_model.name = "My_First_Model"
+        my_model.my_number = 89
+        all_objs = storage.all()
+        my_model.save()
+        with open('file.json', 'r') as f:
+            content = json.load(f)
+            key = "{}.{}".format(type(my_model).__name__, my_model.id)
+            self.assertEqual(content[key]['name'], my_model.name)
+            self.assertEqual(all_objs, all_objs)
             
 
-    def test_filestorage_all(self):
-            storage = FileStorage()
-            obj1 = BaseModel()
-            obj1.save()
-            obj2 = BaseModel()
-            obj2.save()
-            obj3 = BaseModel()
-            obj3.save()
-            all_objs = storage.all()
-            self.assertEqual(all_objs , all_objs)
+    
