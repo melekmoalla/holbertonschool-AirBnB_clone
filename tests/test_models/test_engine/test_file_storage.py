f1820410b1fc1340bdd9fd5639c0f18e520b0d6c
melekmoalla <melekmoalla2468@gmail.com>
2023-02-23 20:09:19 +0100

    test if switch on

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -16,7 +16,7 @@ class TestFileStorage(unittest.TestCase):
         my_model.name = "My_First_Model"
         my_model.my_number = 89
         my_model.save()
-        self.assertEqual(str(my_model), str(my_model))
+        self.assertEqual(str(my_model), "My_First_Model")
 
     def test_all_FileStorage(self):
         self.assertEqual(storage.all(), storage.all())
