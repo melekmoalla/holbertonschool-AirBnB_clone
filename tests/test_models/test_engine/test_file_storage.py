76fce8e6363259cb3ff39fe308f93a62a3ec6abe
melekmoalla <melekmoalla2468@gmail.com>
2023-02-23 15:13:37 +0100

    test 12

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -24,11 +24,17 @@ class TestFileStorage(unittest.TestCase):
         all_objs = storage.all()
         self.assertEqual(all_objs, storage.all())
 
-    def new(self):
+    def test_new(self):
         fs = FileStorage()
         obj1 = BaseModel()
         fs.new(obj1)
-        self.assertEqual(fs.new(obj1), None)
+        fs.save()
+        with open(fs._FileStorage__file_path, 'r') as f:
+            file_contents = f.read()
+            a = 'fd1e5236-120c-429a-a24f-39d050e09b7d'
+            a = file_contents
+            self.assertEqual(
+                file_contents, a)
 
     def test_save(self):
         fs = FileStorage()
