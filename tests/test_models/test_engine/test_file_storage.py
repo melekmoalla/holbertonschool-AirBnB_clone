172f23efd7a93d1803400b50202d0f9cc24167b1
melekmoalla <melekmoalla2468@gmail.com>
2023-02-24 00:28:42 +0100

    test list test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -27,13 +27,11 @@ class TestFileStorage(unittest.TestCase):
         my_model = BaseModel()
         my_model.name = "My_First_Model"
         my_model.my_number = 89
-
+        self.assertEqual(storage.reload(), storage.reload())
+        self.assertEqual(storage.all(), storage.all())
         my_model.save()
 
         with open('file.json', 'r') as f:
             content = json.load(f)
             key = "{}.{}".format(type(my_model).__name__, my_model.id)
             self.assertEqual(content[key]['name'], my_model.name)
-
-        self.assertEqual(storage.reload(), storage.reload())
-        self.assertEqual(storage.all(), storage.all())
