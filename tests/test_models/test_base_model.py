6801ee78bee9074e883d6fa7ce6acbdcdfb98b43
melekmoalla <melekmoalla2468@gmail.com>
2023-02-23 14:29:46 +0100

    test hhh check is easy hhhhhhhhhhh

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -16,5 +16,13 @@ class testBase_AirBnB(unittest.TestCase):
         my_model_json = my_model.to_dict()
         m="{'my_number': 89, 'name': 'My First Model', 'updated_at': '2023-02-20 14:24:01.871493', 'id': '853d2737-0bc7-4dbe-a403-b3934a3de25c', 'created_at': '2023-02-20 14:24:01.871499', '__class__': 'BaseModel'}"
 
-        self.assertEqual((my_model_json), my_model_json)
+        self.assertEqual((my_model_json), "{'my_number': 89, 'name': 'My First Model', 'updated_at': '2023-02-20 14:24:01.871493', 'id': '853d2737-0bc7-4dbe-a403-b3934a3de25c', 'created_at': '2023-02-20 14:24:01.871499', '__class__': 'BaseModel'}")
 
+    def test_to_dict(self):
+        my_model = BaseModel()
+        my_model.name = "My First Model"
+        my_model.my_number = 89
+        my_model_json = my_model.to_dict()
+        m="{'my_number': 89, 'name': 'My First Model', 'updated_at': '2023-02-20 14:24:01.871493', 'id': '853d2737-0bc7-4dbe-a403-b3934a3de25c', 'created_at': '2023-02-20 14:24:01.871499', '__class__': 'BaseModel'}"
+
+        self.assertEqual((my_model_json), my_model_json)
