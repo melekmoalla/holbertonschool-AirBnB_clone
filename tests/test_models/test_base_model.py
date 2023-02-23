e434b946f6285216827b62765c4c2badee081a49
melekmoalla <melekmoalla2468@gmail.com>
2023-02-23 17:56:22 +0100

    test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -16,8 +16,6 @@ class testBase_AirBnB(unittest.TestCase):
         self.assertEqual(
             my_model_json, "{'my_number': 89, 'name': 'My First Model', 'updated_at': '2023-02-20 14:24:01.871493', 'id': '853d2737-0bc7-4dbe-a403-b3934a3de25c', 'created_at': '2023-02-20 14:24:01.871499', '__class__': 'BaseModel'}")
 
-    def test_base(self):
+    def test_save(self):
         my_model = BaseModel()
-        assert my_model.id == my_model.id
-        assert my_model.save() == my_model.save()
-        assert my_model.to_dict() == my_model.to_dict()
+        self.assertEqual(my_model.save(), None)
