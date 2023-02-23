4d48b178069267d53f1f8db5cfa4428794739855
melekmoalla <melekmoalla2468@gmail.com>
2023-02-23 12:39:12 +0100

    test fix documentation

--- models/base_model.py
+++ models/base_model.py
@@ -10,15 +10,22 @@ import models
 class BaseModel:
     """
     * Public instance attributes:
-        - id: string - assign with an uuid when an instance is created:
-        - you can use uuid.uuid4() to generate unique id but don’t forget
-        to convert to a string
-        - the goal is to have unique id for each BaseModel
-        - created_at: datetime - assign with the current datetime when an 
-        instance is created
-        - updated_at: datetime - assign with the current datetime
-        when an instance is created and it
-    *will be updated every time you change your object
+        * you will use *args, **kwargs arguments for the
+        constructor of a BaseModel.
+        (more information inside the AirBnB clone concept page)args
+        won’t be used
+        * if kwargs is not empty:
+        * each key of this dictionary is an attribute name
+        (Note __class__ from kwargs
+        is the only one that should not be added as an attribute.
+        See the example output, below)
+        * each value of this dictionary is the value of this attribute name
+        * Warning: created_at and updated_at are strings in this dictionary,
+        but inside your BaseModel instance is working with datetime object.
+        You have to convert these strings into datetime object. Tip: you know
+        the string format of these datetime
+        otherwise:
+        * create id and created_at as you did previously (new instance)
     """
 
     def __init__(self, name="", my_number=0, *args, **kwargs):
@@ -42,6 +49,9 @@ class BaseModel:
             models.storage.new(self)
 
     def __str__(self):
+        """
+        __str__: should print: [<class name>] (<self.id>) <self.__dict__>"
+        """
         return (f"[BaseModel] ({self.id}) {self.__dict__}")
 
     def save(self):
@@ -54,17 +64,19 @@ class BaseModel:
 
     def to_dict(self):
         """
-        : returns a dictionary containing all keys/values of __dict__ of the instance:
-    by using self.__dict__, only instance attributes set will be returned
-    a key __class__ must be added to this dictionary with the class name of the object
-    created_at and updated_at must be converted to string object in ISO format:
-    format: %Y-%m-%dT%H:%M:%S.%f (ex: 2017-06-14T22:31:03.285259)you can use
-    isoformat() of datetime object
-    """
+        : returns a dictionary containing all keys/values of
+         __dict__ of the instance:
+        by using self.__dict__, only instance attributes set will be returned
+        a key __class__ must be added to this dictionary with the class name of
+        the object
+        created_at and updated_at must be converted to string object
+        in ISO format:
+        format: %Y-%m-%dT%H:%M:%S.%f (ex: 2017-06-14T22:31:03.285259)you
+        can use
+        isoformat() of datetime object
+        """
         dict = self.__dict__.copy()
         dict["updated_at"] = (self.updated_at.isoformat())
-        dict["__class__"] = __class__.__name__
+        dict["__class__"] = self.__class__.__name__
         dict["created_at"] = (self.created_at.isoformat())
         return (dict)
-
-    
