239527a1d1f7a577b49a4e3fb6f0b8d84304177f
melekmoalla <melekmoalla2468@gmail.com>
2023-02-23 17:01:10 +0100

    test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -6,16 +6,14 @@ from models.base_model import BaseModel
 
 
 class TestFileStorage(unittest.TestCase):
-    def test_file_path(self):
+
+    def test_FileStorage(self):
 
         all_objs = storage.all()
-        
+
         my_model = BaseModel()
         my_model.name = "My_First_Model"
         my_model.my_number = 89
         my_model.save()
         self.assertEqual(str(my_model), str(my_model))
 
-    def test_storage(self):
-        storage.new(BaseModel())
-        print(storage)
